06918a597574e002ca7fe77d0ce29972261f2ab5
thomasklein-git <thomas.frank.klein@gmail.com>
2020-09-23 11:54:02 +0000

    Prepared for zed

--- src/testsrc/camera/src/imagenet.py
+++ src/testsrc/camera/src/imagenet.py
@@ -12,11 +12,11 @@ from cv_bridge import CvBridge, CvBridgeError
 class image_converter:
 
   def __init__(self):
-    #self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.image_pub = rospy.Publisher("image_topic_2",Image)
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
-    #self.image_sub = rospy.Subscriber("/zed2/zed_node/left/image_rect_color",Image,self.callback)
+    #self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+    self.image_sub = rospy.Subscriber("/zed2/zed_node/left/image_rect_color",Image,self.callback)
     
 
   def callback(self,data):
