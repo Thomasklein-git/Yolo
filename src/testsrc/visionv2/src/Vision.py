95ba488d7392c85a947696a98faa28b9a45e8210
Jakobine <jakobvks@gmail.com>
2020-10-02 17:25:09 +0200

    Vision added

---
+++ src/testsrc/visionv2/src/Vision.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3.8
+import roslib
+import sys
+import rospy
+import cv2
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from yolov3.utils import detect_image, Load_Yolo_model
+from yolov3.configs import *
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+class image_converter:
+
+  def __init__(self):
+    global yolo
+    self.image_pub = rospy.Publisher("image_topic_2",Image)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
+    #self.image_sub = rospy.Subscriber("/zed2/zed_node/left/image_rect_color",Image,self.callback)
+    yolo = Load_Yolo_model()
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+
+    ####
+    cv_image=detect_image(yolo, cv_image, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
+    #cv_image=detect_image(yolo, cv_image, "", input_size=YOLO_INPUT_SIZE, show=False, CLASSES=TRAIN_CLASSES, rectangle_colors=(255,0,0)) # Used later for custom weigths
+    ####
+
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+    
+
+def main(args):
+  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
