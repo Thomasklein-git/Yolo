4c38be18d64f65162e150db470c67a40eb1a0455
thomasklein-git <thomas.frank.klein@gmail.com>
2020-10-12 11:25:52 +0200

    Frank video

--- src/testsrc/visionv2/src/Web_test.py
+++ src/testsrc/visionv2/src/Web_test.py
@@ -11,7 +11,7 @@ video_path   = "./model_data/test.mp4"
 
 yolo = Load_Yolo_model()
 #detect_image(yolo, image_path, "./IMAGES/kite_pred.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-detect_video(yolo, video_path, "./model_data/test_pred_Jakob_CPU.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
+detect_video(yolo, video_path, "./model_data/test_pred_FRANK_CPU.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
 #detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
 
 #detect_video_realtime_mp(video_path, "./model_data/test_pred.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
