f4bf28c5f81a7a8a75317aa647bcae250b9a563d
thomasklein-git <thomas.frank.klein@gmail.com>
2020-11-04 13:09:00 +0100

    yolo

--- src/yolo/src/yolo_listener_test.py
+++ src/yolo/src/yolo_listener_test.py
@@ -5,26 +5,30 @@ import cv2
 import message_filters
 from cv_bridge import CvBridge, CvBridgeError
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage, Image
 from vision_msgs.msg import Detection2DArray
 
 
 class yolo_listener:
     def __init__(self):
         self.bridge = CvBridge()
-        image_sub  = message_filters.Subscriber("/zed2/zed_node/left/image_rect_color",Image)
-        detect_sub = message_filters.Subscriber("/yolo/bboxes",Detection2DArray)
+        image_sub   = message_filters.Subscriber("/zed2/zed_node/left/image_rect_color/compressed",CompressedImage)
+        detect_sub  = message_filters.Subscriber("/yolo/bboxes",Detection2DArray)
 
+        self.boximg_pub  = rospy.Publisher("/yolo/boxes_image",Image,queue_size=1)
+
+        self.timing = 0
         #mf = message_filters.ApproximateTimeSynchronizer([image_sub,detect_sub],10,0.07)
-        mf = message_filters.TimeSynchronizer([image_sub,detect_sub],100)
+        mf = message_filters.TimeSynchronizer([image_sub,detect_sub],queue_size=30)
         mf.registerCallback(self.callback)
+        
 
 
 
     def callback(self, image, detect):
-        cv_image = self.bridge.imgmsg_to_cv2(image, image.encoding)
-        print("msg")
-        """
+        self.timing += 1
+        print("timing?", self.timing)
+        cv_image = self.bridge.compressed_imgmsg_to_cv2(image, "bgr8")
         for Object in detect.detections:
             Start_x = Object.bbox.center.x - Object.bbox.size_x/2
             End_x   = Object.bbox.center.x + Object.bbox.size_x/2
@@ -32,16 +36,8 @@ class yolo_listener:
             End_y   = Object.bbox.center.y + Object.bbox.size_y/2
 
             cv2.rectangle(cv_image, (int(Start_x), int(Start_y)), (int(End_x), int(End_y)), (0, 0, 255), 2)
-        cv2.imshow("Image_window", cv_image)
-        cv2.waitKey(1)
-        """
-        
-
-
-
-
-    
-
+        boximg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+        self.boximg_pub.publish(boximg)
 
 def main(args):
 	rospy.init_node('YOLO_listener', anonymous=True)
