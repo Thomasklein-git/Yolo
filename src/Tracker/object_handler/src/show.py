006d9b8d628dd66e76ae439c64725b3e93ebeed0
Jakobine <jakobvks@gmail.com>
2020-12-03 15:53:25 +0100

    images

--- src/Tracker/object_handler/src/show.py
+++ src/Tracker/object_handler/src/show.py
@@ -62,8 +62,7 @@ class show:
 
             Position    = "X: {}, Y: {}, Z: {}".format(Posx,Posy,Posz)
             Description = "Class: {}, ID {}".format(Class, UID)
-            
-            
+
             cv2.rectangle(cv_image, (int(Start_x),int(Start_y)), (int(End_x),int(End_y)), color, 1)
             
             cv2.putText(cv_image, Position,     (int(box.bbox.center.x), int(box.bbox.center.y)-5), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
